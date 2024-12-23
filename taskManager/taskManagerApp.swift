cbd35061482aabd973ab7fe1c3ec6c059235e435
cryozeus <yashbhardwaj911@gmail.com>
2024-12-23 18:48:34 +0530

    Initial Commit

---
+++ taskManager/taskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskManagerApp.swift
+//  taskManager
+//
+//  Created by Yash Bhardwaj on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct taskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ taskManagerTests/taskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  taskManagerTests.swift
+//  taskManagerTests
+//
+//  Created by Yash Bhardwaj on 23/12/24.
+//
+
+import Testing
+@testable import taskManager
+
+struct taskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
